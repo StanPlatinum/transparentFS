f445ceff9b8b7320a6aed051c2f63a3eba0037e2
StanPlatinum <lpjlwjlpjlwj@126.com>
2021-04-01 19:52:28 -0400

    add sgx pf demo

--- pf-server/Enclave/Enclave.cpp
+++ pf-server/Enclave/Enclave.cpp
@@ -6,6 +6,8 @@
 
 #include "string.h"
 
+#include "TrustedPF/protected_files.h"
+
 /* re-define printf inside enclave */
 void printf(const char *fmt, ...){
 	char buf[BUFSIZ] = {'\0'};
@@ -17,5 +19,9 @@ void printf(const char *fmt, ...){
 }
 
 int pf_demo_processing(void){
+	//     pf_handle_t handle = (pf_handle_t)&output;
+    // pf_status_t pfs = pf_open(handle, output_path, /*size=*/0, PF_FILE_MODE_WRITE, /*create=*/true,
+    //                           wrap_key, &pf);
+
     return 0;
 }
